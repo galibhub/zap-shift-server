cb7cca4297ac4bd3da91e1b265cb731fc1af9a55
Ibrahim Ahmed Galib <galib@Ibrahims-Mac-mini.local>
2025-11-28 12:37:48 +0600

    Created collections for parcel and get and post apies

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 const express = require('express')
+import { async } from './node_modules/mongodb/src/client-side-encryption/providers/azure';
 const cors=require('cors')
 const app = express()
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000
 
 
 //middleware
 app.use(express.json());
 app.use(cors());
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@web-projects.djmog22.mongodb.net/?appName=web-projects`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //create a collection for zap-shift-projects
+
+    const db=client.db('zap_shift_db');
+    const parcelsCollection=db.collection('parcels');
+
+
+    //------------parcels APIes--------------------
+
+
+    //get api
+    app.get('/parcels',async(req,res)=>{
+
+    })
+
+    //post api
+
+    app.post('/parcels',async(req,res)=>{
+        const parcel=req.body;
+        const result=await parcelsCollection.insertOne(parcel);
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Zap is Shifting Shifting!')
