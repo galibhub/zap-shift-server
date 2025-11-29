10ccb8f2375e2e39e038fb7185281a7a1a4a75d3
Ibrahim Ahmed Galib <galib@Ibrahims-Mac-mini.local>
2025-11-29 10:26:45 +0600

    Created STRIPE Payment getway API

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express')
+const express = require("express");
 
-const cors=require('cors')
-const app = express()
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 3000
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
 app.use(cors());
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@web-projects.djmog22.mongodb.net/?appName=web-projects`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@web-projects.djmog22.mongodb.net/?appName=web-projects`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,60 +29,91 @@ async function run() {
 
     //create a collection for zap-shift-projects
 
-    const db=client.db('zap_shift_db');
-    const parcelsCollection=db.collection('parcels');
-
+    const db = client.db("zap_shift_db");
+    const parcelsCollection = db.collection("parcels");
 
     //------------parcels APIes--------------------
 
-
     //get api
-    app.get('/parcels',async(req,res)=>{
-     const query={}
-     const {email}=req.query;
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
 
-     if(email){
-        query.senderEmail=email;
-     }
+      if (email) {
+        query.senderEmail = email;
+      }
 
-    const options={sort:{createdAt:-1}}
+      const options = { sort: { createdAt: -1 } };
 
-     const cursor=parcelsCollection.find(query,options);
-     const result=await cursor.toArray();
-     res.send(result)
-    })
+      const cursor = parcelsCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     //post api
 
-    app.post('/parcels',async(req,res)=>{
-        const parcel=req.body;
-        //parcel created time
-        parcel.createdAt=new Date();
-        const result=await parcelsCollection.insertOne(parcel);
-        res.send(result)
-    })
-
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      //parcel created time
+      parcel.createdAt = new Date();
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
 
     //delete api
-    app.delete('/parcels/:id',async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
-      const result=await parcelsCollection.deleteOne(query);
+      const result = await parcelsCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
+    //stripe payment getway
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
 
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "USD",
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.parcelName,
+              },
+            },
 
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.senderEmail,
+        mode: "payment",
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+        },
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+      console.log(session)
+      res.send({url:session.url})
+    });
 
+    //get api for pay specific parcel
 
-
-
-
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -89,14 +121,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Zap is Shifting Shifting!')
-})
+app.get("/", (req, res) => {
+  res.send("Zap is Shifting Shifting!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
